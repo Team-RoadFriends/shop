1fe0cb0d82f428d1a8f87f1928cd75b6130fdc82
viwk <tmsn1636@gmail.com>
2025-01-24 13:45:25 +0900

    아이템 등록페이지

---
+++ src/main/java/com/RoadShop/shopping/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.RoadShop.shopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping("/create")
+    public String create(){
+        return "shop/create-product";
+    }
+}
