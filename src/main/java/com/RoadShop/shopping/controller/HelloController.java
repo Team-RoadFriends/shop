6773e879888ca1bebaafec7a8bc4ea4e14bb567a
seo minhee <minheee07@gmail.com>
2025-01-24 11:21:36 +0900

    프로젝트 생성

---
+++ src/main/java/com/RoadShop/shopping/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.RoadShop.shopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+  @GetMapping("/hello")
+  public String hello(){
+    return "common/hello";
+  }
+}
